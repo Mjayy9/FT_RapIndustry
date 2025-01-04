ccb53499ce1463e6b35cc648c8071a634827ed09
Majkyyy <marian.sebora9@gmail.com>
2025-01-04 18:31:50 +0100

    Úpravy na stránke

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "NavigationBar"
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
